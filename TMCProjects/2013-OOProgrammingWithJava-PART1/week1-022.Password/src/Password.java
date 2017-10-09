330abbd0870e6ff889a6a07b205cf748b940469f
Aman Bahata <bahataaman@gmail.com>
2017-10-09 21:24:18 +0100

    Adds solution to exercise.

--- TMCProjects/2013-OOProgrammingWithJava-PART1/week1-022.Password/src/Password.java
+++ TMCProjects/2013-OOProgrammingWithJava-PART1/week1-022.Password/src/Password.java
@@ -6,7 +6,22 @@ public class Password {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         String password = "carrot"; // Use carrot as password when running tests.
+        String input;
+        String secret = "Secret message!";
 
         // Write your code here
+
+
+        System.out.print("Type the password: ");
+        input = reader.nextLine();
+        while (password.compareTo(input) != 0){
+            System.out.println("Wrong!");
+            System.out.print("Type the password: ");
+            input = reader.nextLine();
+        }
+        System.out.println("Right!");
+        System.out.println("The secret is : " + secret);
+
+
     }
 }
