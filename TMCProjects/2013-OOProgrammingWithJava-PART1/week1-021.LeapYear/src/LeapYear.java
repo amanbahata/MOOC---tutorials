b8bf3532d03bf135bd30262ea478a28a40c1a274
Aman Bahata <bahataaman@gmail.com>
2017-10-09 20:49:13 +0100

    Adds LeapYear exercise

--- TMCProjects/2013-OOProgrammingWithJava-PART1/week1-021.LeapYear/src/LeapYear.java
+++ TMCProjects/2013-OOProgrammingWithJava-PART1/week1-021.LeapYear/src/LeapYear.java
@@ -6,5 +6,25 @@ public class LeapYear {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
+        System.out.print("Type a year: ");
+        int year = Integer.parseInt(reader.next());
+
+        boolean isLeapYear = false;
+
+        if (year % 4 == 0) {
+            isLeapYear = true;
+        }
+        if (year % 100 == 0){
+            isLeapYear = false;
+            if (year % 400 == 0){
+                isLeapYear = true;
+            }
+        }
+
+        if (isLeapYear){
+            System.out.println("The year is a leap year.");
+        }else{
+            System.out.println("The year is not leap year.");
+        }
     }
 }
