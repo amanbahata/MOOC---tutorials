8c19fbe74a1d9295c86ac01c4fa5d3ed7c58b548
Aman Bahata <bahataaman@gmail.com>
2017-10-11 12:01:32 +0100

    Adds solution to EvenNumbers exercise

--- TMCProjects/2013-OOProgrammingWithJava-PART1/week2-029.EvenNumbers/src/EvenNumbers.java
+++ TMCProjects/2013-OOProgrammingWithJava-PART1/week2-029.EvenNumbers/src/EvenNumbers.java
@@ -3,5 +3,12 @@ public class EvenNumbers {
 
     public static void main(String[] args) {
         // Write your code here
+        int i = 2;
+        while ( i <= 100){
+            if (i%2 ==0) {
+                System.out.println(i);
+            }
+            i++;
+        }
     }
 }
