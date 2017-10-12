9046f7d45a00fafc3b44637a8cf3adf05f1fc7f1
Aman Bahata <bahataaman@gmail.com>
2017-10-12 22:42:14 +0100

    Adds solution to Factorial exercise

--- TMCProjects/2013-OOProgrammingWithJava-PART1/week2-034.Factorial/src/Factorial.java
+++ TMCProjects/2013-OOProgrammingWithJava-PART1/week2-034.Factorial/src/Factorial.java
@@ -4,5 +4,21 @@ public class Factorial {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
+        int sum = 1;
+        int counter = 1;
+
+        System.out.println("Type a number: ");
+        int input = Integer.parseInt(reader.next());
+
+        if (input == 0){
+            sum = 1;
+        }else {
+            while (counter <= input){
+                sum = sum * counter;
+                counter++;
+            }
+        }
+        System.out.println("Factorial is " + sum);
+
     }
 }
