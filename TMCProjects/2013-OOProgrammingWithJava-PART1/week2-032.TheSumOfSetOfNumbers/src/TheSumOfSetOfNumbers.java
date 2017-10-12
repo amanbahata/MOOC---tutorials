a2eb177e7c9700a8504c9fe0153fe31a0189b6cc
Aman Bahata <bahataaman@gmail.com>
2017-10-12 19:29:52 +0100

    Adds solution to exercise TheSumOfNumbers

--- TMCProjects/2013-OOProgrammingWithJava-PART1/week2-032.TheSumOfSetOfNumbers/src/TheSumOfSetOfNumbers.java
+++ TMCProjects/2013-OOProgrammingWithJava-PART1/week2-032.TheSumOfSetOfNumbers/src/TheSumOfSetOfNumbers.java
@@ -6,5 +6,16 @@ public class TheSumOfSetOfNumbers {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
+        System.out.print("Until what?: ");
+        int input = Integer.parseInt(reader.next());
+        int sum = 0;
+        int counter = 0;
+
+        while (counter <= input){
+            sum += counter;
+            counter++;
+        }
+        System.out.println("Sum is: " + sum);
+
     }
 }
