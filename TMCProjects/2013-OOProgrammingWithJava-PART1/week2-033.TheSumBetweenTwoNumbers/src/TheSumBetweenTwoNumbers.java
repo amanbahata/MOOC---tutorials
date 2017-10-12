3e5e604f6fd2e98161620479f8977487ce6cff71
Aman Bahata <bahataaman@gmail.com>
2017-10-12 22:29:07 +0100

    Adds solution to the exercise TheSumBetweenTwoNumbers

--- TMCProjects/2013-OOProgrammingWithJava-PART1/week2-033.TheSumBetweenTwoNumbers/src/TheSumBetweenTwoNumbers.java
+++ TMCProjects/2013-OOProgrammingWithJava-PART1/week2-033.TheSumBetweenTwoNumbers/src/TheSumBetweenTwoNumbers.java
@@ -5,6 +5,20 @@ import java.util.Scanner;
 public class TheSumBetweenTwoNumbers {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        
+
+        System.out.print("First: ");
+        int first = Integer.parseInt(reader.next());
+        System.out.print("Last: ");
+        int last = Integer.parseInt(reader.next());
+
+        int counter = first;
+        int sum = 0;
+
+        while (counter <= last){
+            sum += counter;
+            counter++;
+        }
+        System.out.println("The sum is " + sum);
+
     }
 }
