4ed2201e1a4fce1a915250c8258964533459b1e7
Aman Bahata <bahataaman@gmail.com>
2017-10-13 20:02:18 +0100

    Adds solution to SumOfPowers exercise

--- TMCProjects/2013-OOProgrammingWithJava-PART1/week2-035.SumOfThePowers/src/SumOfThePowers.java
+++ TMCProjects/2013-OOProgrammingWithJava-PART1/week2-035.SumOfThePowers/src/SumOfThePowers.java
@@ -5,6 +5,18 @@ public class SumOfThePowers {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
+        System.out.print("Type a number: ");
+        int input = Integer.parseInt(reader.nextLine());
+
+        int sum = 0;
+        int counter = 0;
+
+        while (counter <= input){
+            sum += (int) Math.pow(2, counter);
+            counter++;
+        }
+
+        System.out.println("The result is: " + sum);
 
     }
 }
