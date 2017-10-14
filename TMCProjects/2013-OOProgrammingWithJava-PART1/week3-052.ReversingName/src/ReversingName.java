d6df8ddcbc065d53d327b9b6ab265878813ac540
Aman Bahata <bahataaman@gmail.com>
2017-10-14 22:56:06 +0100

    Adds solution to exercise ReversingName

--- TMCProjects/2013-OOProgrammingWithJava-PART1/week3-052.ReversingName/src/ReversingName.java
+++ TMCProjects/2013-OOProgrammingWithJava-PART1/week3-052.ReversingName/src/ReversingName.java
@@ -3,5 +3,17 @@ import java.util.Scanner;
 public class ReversingName {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
+        System.out.print("Type your name: ");
+        String input = reader.nextLine();
+        System.out.println("In reverse order: " + textReverse(input));
+    }
+
+    public static String textReverse(String text){
+        String container = "";
+
+        for (int i = text.length() - 1;i >= 0; i-- ){
+            container = container + text.charAt(i);
+        }
+        return container;
     }
 }
