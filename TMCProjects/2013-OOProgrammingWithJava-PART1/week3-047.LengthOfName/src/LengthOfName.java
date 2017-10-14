c67703c949a1027aaddb7e1bd3db7535bc049857
Aman Bahata <bahataaman@gmail.com>
2017-10-14 22:07:37 +0100

    Adds solution to exercise LengthOfName

--- TMCProjects/2013-OOProgrammingWithJava-PART1/week3-047.LengthOfName/src/LengthOfName.java
+++ TMCProjects/2013-OOProgrammingWithJava-PART1/week3-047.LengthOfName/src/LengthOfName.java
@@ -5,9 +5,14 @@ public class LengthOfName {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         // call your method from here
+        System.out.print("Type your name: ");
+        String input = reader.nextLine();
+        System.out.println("Number of characters: " + calculateCharacters(input));
     }
     
     // do here the method
-    // public static int calculateCharacters(String text)
+    public static int calculateCharacters(String text){
+        return text.length();
+    }
     
 }
