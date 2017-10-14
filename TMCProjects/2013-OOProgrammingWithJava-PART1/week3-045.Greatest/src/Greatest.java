69e94009b3e2982516ea3da12ecff975921d5ec5
Aman Bahata <bahataaman@gmail.com>
2017-10-14 21:50:35 +0100

    Adds solution to exercise Greatest

--- TMCProjects/2013-OOProgrammingWithJava-PART1/week3-045.Greatest/src/Greatest.java
+++ TMCProjects/2013-OOProgrammingWithJava-PART1/week3-045.Greatest/src/Greatest.java
@@ -3,7 +3,7 @@ public class Greatest {
 
     public static int greatest(int number1, int number2, int number3) {
 
-        return -1;
+        return Math.max(Math.max(number1, number2),number3);
     }
 
     public static void main(String[] args) {
