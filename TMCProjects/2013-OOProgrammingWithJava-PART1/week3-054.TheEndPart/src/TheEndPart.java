efc808e499af930bd052ba21ed2bfb9a4370bf34
Aman Bahata <bahataaman@gmail.com>
2017-10-15 22:00:50 +0100

    Adds solution to TheEndPart exercise

--- TMCProjects/2013-OOProgrammingWithJava-PART1/week3-054.TheEndPart/src/TheEndPart.java
+++ TMCProjects/2013-OOProgrammingWithJava-PART1/week3-054.TheEndPart/src/TheEndPart.java
@@ -3,5 +3,17 @@ import java.util.Scanner;
 public class TheEndPart {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
+
+        System.out.print("Type a word: ");
+        String word = reader.nextLine();
+
+        System.out.print("Length of the end part: ");
+        int length = Integer.parseInt(reader.nextLine());
+
+        if (length < word.length()){
+            System.out.println("Result: " + word.substring(word.length() - length));
+        }else {
+            throw new IndexOutOfBoundsException("The length entered is greater than the word itself");
+        }
     }
 }
