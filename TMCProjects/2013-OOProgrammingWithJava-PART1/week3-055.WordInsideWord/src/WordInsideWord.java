498c6aeb5e3fb628071c2bbd2bb57a8cb2dc8fbb
Aman Bahata <bahataaman@gmail.com>
2017-10-15 23:03:22 +0100

    Adds solution to exercise WordInsideWord exercise

--- TMCProjects/2013-OOProgrammingWithJava-PART1/week3-055.WordInsideWord/src/WordInsideWord.java
+++ TMCProjects/2013-OOProgrammingWithJava-PART1/week3-055.WordInsideWord/src/WordInsideWord.java
@@ -5,5 +5,32 @@ public class WordInsideWord {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
+
+        System.out.print("Type the first word: ");
+        String firstWord = reader.nextLine();
+        System.out.print("Type the second word: ");
+        String secondWord = reader.nextLine();
+
+        boolean found  = false;
+        int j = 0;
+        int i = firstWord.indexOf(secondWord.charAt(j));
+
+        while (j < secondWord.length()){
+            if (firstWord.charAt(i) == secondWord.charAt(j)){
+                found = true;
+                i++;
+                j++;
+            }else{
+                found = false;
+                break;
+            }
+        }
+
+        if (found){
+            System.out.print("The word \'" + secondWord  + "\' is found in the word \'" + firstWord + "\'");
+        }else {
+            System.out.print("The word \'" + secondWord  + "\' is not found in the word \'" + firstWord + "\'");
+        }
+
     }
 }
