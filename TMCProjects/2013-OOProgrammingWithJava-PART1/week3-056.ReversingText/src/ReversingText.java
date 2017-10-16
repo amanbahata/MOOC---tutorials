c930b55f1e67cd53d7ec77c44bf560b302d272b9
Aman Bahata <bahataaman@gmail.com>
2017-10-16 19:15:58 +0100

    Adds solution to ReversingText exercise

--- TMCProjects/2013-OOProgrammingWithJava-PART1/week3-056.ReversingText/src/ReversingText.java
+++ TMCProjects/2013-OOProgrammingWithJava-PART1/week3-056.ReversingText/src/ReversingText.java
@@ -6,7 +6,12 @@ public class ReversingText {
     public static String reverse(String text) {
         // write your code here
         // note that method does now print anything, it RETURNS the reversed string
-        return "";
+        String reverse = "";
+        for (int i = text.length() - 1; i >= 0; i--){
+            reverse += text.charAt(i);
+        }
+
+        return reverse;
     }
 
     public static void main(String[] args) {
