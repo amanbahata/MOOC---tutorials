2964ab783263c6264805deb8c56b7d922ee79d8e
Aman Bahata <bahataaman@gmail.com>
2017-10-16 20:41:50 +0100

    Adds solution to exercise RecurringWord

--- TMCProjects/2013-OOProgrammingWithJava-PART1/week3-058.RecurringWord/src/RecurringWord.java
+++ TMCProjects/2013-OOProgrammingWithJava-PART1/week3-058.RecurringWord/src/RecurringWord.java
@@ -6,7 +6,24 @@ public class RecurringWord {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        // create here the ArrayList 
+        // create here the ArrayList
+
+        ArrayList<String> words = new ArrayList<String>();
+
+
+        System.out.print("Type a word: ");
+        String in = reader.nextLine();
+
+        while (!in.isEmpty()){
+            if (words.contains(in)){
+                System.out.println("You gave the word " + in + " twice");
+                break;
+            }else {
+                words.add(in);
+            }
+            System.out.print("Type a word: ");
+            in = reader.nextLine();
+        }
         
     }
 }
