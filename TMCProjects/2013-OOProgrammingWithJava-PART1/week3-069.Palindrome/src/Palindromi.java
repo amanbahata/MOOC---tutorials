734779a6af691ae6f7b84f71172fadbb6b8e1475
Aman Bahata <bahataaman@gmail.com>
2017-10-20 23:43:33 +0100

    Adds solution to exercise Palindrome

--- TMCProjects/2013-OOProgrammingWithJava-PART1/week3-069.Palindrome/src/Palindromi.java
+++ TMCProjects/2013-OOProgrammingWithJava-PART1/week3-069.Palindrome/src/Palindromi.java
@@ -3,14 +3,34 @@ import java.util.Scanner;
 public class Palindromi {
 
     public static boolean palindrome(String text) {
-        // write code here
-        return false;
+
+        int middle = text.length() / 2;
+        int j = text.length() - 1;
+        boolean flag = false;
+        int i = 0;
+
+        if (text.isEmpty() || text.length() == 1 ){
+            return true;
+        }
+
+        while (i < middle){
+            if (text.charAt(i) == text.charAt(j)){
+                flag = true;
+                i++;
+                j--;
+            }else {
+                flag = false;
+                break;
+            }
+        }
+
+        return flag;
     }
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         
-        System.out.println("Type a text: ");
+        System.out.print("Type a text: ");
         String text = reader.nextLine();    
         if (palindrome(text)) {
             System.out.println("The text is a palindrome!");
