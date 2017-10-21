67fbad326690765140dd55e608da95d005481292
Aman Bahata <bahataaman@gmail.com>
2017-10-21 17:59:03 +0100

    Adds exercise list for week four

---
+++ TMCProjects/2013-OOProgrammingWithJava-PART1/week4-072.1.YourFirstAccount/src/Accounts.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+public class Accounts {
+
+    public static void main(String[] args) {
+        // Code in Account.Java should not be touched!
+        Account account1 = new Account("Aman", 100.00);
+        account1.deposit(20.00);
+        System.out.println(account1);
+    }
+
+}
