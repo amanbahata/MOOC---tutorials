c28336591f91755a6df0734fe17376c6aac18f3e
Aman Bahata <bahataaman@gmail.com>
2017-10-22 18:03:47 +0100

    Adds solution to exercise LyyraCard

---
+++ TMCProjects/2013-OOProgrammingWithJava-PART1/week4-077.LyyraCard/src/LyyraCard.java
@@ -0,0 +1,30 @@
+public class LyyraCard {
+
+    private double balance;
+
+    public LyyraCard(double balanceAtStart){
+        balance = balanceAtStart;
+    }
+
+    public String toString(){
+        return "The card has " + balance + " euros";
+    }
+
+    public void payEconomical(){
+        if (balance >= 2.50)
+            balance -= 2.50;
+    }
+
+    public void payGourmet(){
+        if (balance >= 4.00)
+            balance -= 4.00;
+    }
+
+    public void loadMoney(double amount){
+        if (amount > 0)
+            balance += amount;
+        if (balance > 150){
+            balance = 150;
+        }
+    }
+}
--- TMCProjects/2013-OOProgrammingWithJava-PART1/week4-077.LyyraCard/src/Main.java
+++ TMCProjects/2013-OOProgrammingWithJava-PART1/week4-077.LyyraCard/src/Main.java
@@ -5,7 +5,33 @@ public class Main {
 
     public static void main(String[] args) {
         // add here code that tests LyraCard. However before doing 77.6 remove the
-        // other code 
+        // other code
 
+        LyyraCard cardPekka = new LyyraCard(20.0);
+        LyyraCard cardBrian = new LyyraCard(30.0);
+
+
+        cardPekka.payGourmet();
+        cardBrian.payEconomical();
+
+        System.out.println("Pekka: " + cardPekka);
+        System.out.println("Brian: " + cardBrian);
+
+        loadMoney(cardPekka, 20.0);
+        cardBrian.payGourmet();
+
+        System.out.println("Pekka: " + cardPekka);
+        System.out.println("Brian: " + cardBrian);
+
+        cardPekka.payEconomical();
+        cardPekka.payEconomical();
+
+        loadMoney(cardBrian, 50.0);
+        System.out.println("Pekka: " + cardPekka);
+        System.out.println("Brian: " + cardBrian);
+    }
+
+    public static void loadMoney(LyyraCard card, double amount){
+        card.loadMoney(amount);
     }
 }
