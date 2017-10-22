72e3be81374e9681b1a713d2fff17b73bb8ff402
Aman Bahata <bahataaman@gmail.com>
2017-10-22 12:18:53 +0100

    Adds solution to exercise Multiplier

---
+++ TMCProjects/2013-OOProgrammingWithJava-PART1/week4-074.Multiplier/src/Multiplier.java
@@ -0,0 +1,12 @@
+public class Multiplier {
+
+    private Integer number;
+
+    public Multiplier(int number){
+        this.number = number;
+    }
+
+    public int multiply(int otherNumber){
+        return number * otherNumber;
+    }
+}
