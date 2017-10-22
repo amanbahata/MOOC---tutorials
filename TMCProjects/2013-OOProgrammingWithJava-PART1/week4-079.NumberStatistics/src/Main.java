a2ccda9651e4f56556824513eafd73fbacf62c43
Aman Bahata <bahataaman@gmail.com>
2017-10-22 19:40:55 +0100

    Adds solution to exercise NumberStatistics

--- TMCProjects/2013-OOProgrammingWithJava-PART1/week4-079.NumberStatistics/src/Main.java
+++ TMCProjects/2013-OOProgrammingWithJava-PART1/week4-079.NumberStatistics/src/Main.java
@@ -3,14 +3,29 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        // Write test code here
-        // Remember to remove all the extra code when doing assignments  79.3 and 79.4
+        int input = 0;
 
-        // Define three NumberStatistics objects in your program:
-        // The first is used to track the sum of all given numbers.
-        // The second takes care of even numbers and the third the odd numbers.
+        NumberStatistics stats = new NumberStatistics();
+        NumberStatistics even = new NumberStatistics();
+        NumberStatistics odd = new NumberStatistics();
 
-        // The tests does not work if you do not create the objects in the correct order
+        do {
+            System.out.print("Type numbers: ");
+            input = Integer.parseInt(reader.nextLine());
+            if (input != -1){
+                stats.addNumber(input);
+                if (input % 2 == 0){
+                    even.addNumber(input);
+                }else{
+                    odd.addNumber(input);
+                }
+            }
+        }while (input != -1);
+
+
+        System.out.println("sum: " + stats.sum());
+        System.out.println("sum of even: " + even.sum());
+        System.out.println("sum of odd: " + odd.sum());
 
     }
 }
