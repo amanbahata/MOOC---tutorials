7019a7630b64e8dd621ef08f02a5c49288e774e0
Aman Bahata <bahataaman@gmail.com>
2017-10-23 20:30:44 +0100

    Adds solution to exercise HangmanLogic

--- TMCProjects/2013-OOProgrammingWithJava-PART1/week4-083.HangmanLogic/src/HangmanLogic.java
+++ TMCProjects/2013-OOProgrammingWithJava-PART1/week4-083.HangmanLogic/src/HangmanLogic.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 public class HangmanLogic {
 
@@ -23,13 +24,20 @@ public class HangmanLogic {
         return 12;
     }
 
+    /**
+     * functionality for making a guess. If the letter has already been guessed, nothing happens
+     * it the word does not contains the guessed letter, number of faults increase
+     * the letter is added among the already guessed letters
+     * @param letter
+     */
+
     public void guessLetter(String letter) {
-        // program here the functionality for making a guess
-
-        // if the letter has already been guessed, nothing happens
-
-        // it the word does not contains the guessed letter, number of faults increase
-        // the letter is added among the already guessed letters
+        if (!word.contains(letter) && !guessedLetters.contains(letter)){
+            numberOfFaults++;
+        }
+        if (!guessedLetters.contains(letter)){
+            guessedLetters += letter;
+        }
     }
 
     public String hiddenWord() {
@@ -37,9 +45,18 @@ public class HangmanLogic {
 
         // create the hidden word by interating through this.word letter by letter
         // if the letter in turn is within the guessed words, put it in to the hidden word
-        // if the letter is not among guessed, replace it with _ in the hidden word 
+        // if the letter is not among guessed, replace it with _ in the hidden word
+        String hiddenWord = "";
+        for (int i = 0; i < word.length(); i++){
+            String letter = "" + word.charAt(i);
+            if (guessedLetters.contains(letter)){
+                hiddenWord += letter;
+            }else {
+                hiddenWord += "_";
+            }
+        }
 
         // return the hidden word at the end
-        return "";
+        return hiddenWord;
     }
 }
