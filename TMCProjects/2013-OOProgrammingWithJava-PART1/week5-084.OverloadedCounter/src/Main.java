348ff402d989b2e379ec2d39563820e653e4df5d
Aman Bahata <bahataaman@gmail.com>
2017-10-24 20:56:44 +0100

    Adds solution to exercise OverloadedCounter

--- TMCProjects/2013-OOProgrammingWithJava-PART1/week5-084.OverloadedCounter/src/Main.java
+++ TMCProjects/2013-OOProgrammingWithJava-PART1/week5-084.OverloadedCounter/src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        // write testcode here
+
+        Counter counter1 = new Counter();
+        counter1.decrease();
+        System.out.println(counter1);
+
+
+        Counter counter2 = new Counter(true);
+        counter2.decrease();
+        System.out.println(counter2);
+
+        Counter counter3 = new Counter(5, true);
+        counter3.increase();
+        System.out.println(counter3);
+        counter3.decrease(3);
+        System.out.println(counter3);
+
     }
 }
