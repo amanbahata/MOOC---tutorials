db7106ae485a623cc4858881d1ff9a4bdd302f66
Aman Bahata <bahataaman@gmail.com>
2017-10-24 23:22:52 +0100

    Adds solution to LyyraCardAndRegister

--- TMCProjects/2013-OOProgrammingWithJava-PART1/week5-086.LyyraCardAndRegister/src/CashRegister.java
+++ TMCProjects/2013-OOProgrammingWithJava-PART1/week5-086.LyyraCardAndRegister/src/CashRegister.java
@@ -5,8 +5,12 @@ public class CashRegister {
     private int economicalSold;
     private int gourmetSold;
 
+    private static final double PRICE_OF_ECONOMICAL_LUNCH = 2.50;
+    private static final double PRICE_OF_GOURMET_LUNCH = 4.00;
+
     public CashRegister() {
-        // at start the register has 1000 euros 
+        // at start the register has 1000 euros
+        cashInRegister = 1000;
     }
 
     public double payEconomical(double cashGiven) {
@@ -15,8 +19,15 @@ public class CashRegister {
         //    the price of lunch is added to register
         //    the amount of sold lunch is incremented by one
         //    method returns cashGiven - lunch price 
-        // if not enough money given, all is returned and nothing else happens        
-        return -1;
+        // if not enough money given, all is returned and nothing else happens
+
+        if (cashGiven >= PRICE_OF_ECONOMICAL_LUNCH){
+            this.cashInRegister += PRICE_OF_ECONOMICAL_LUNCH;
+            this.economicalSold++;
+            return cashGiven - PRICE_OF_ECONOMICAL_LUNCH;
+        }
+
+        return cashGiven;
     }
 
     public double payGourmet(double cashGiven) {
@@ -26,9 +37,43 @@ public class CashRegister {
         //    the amount of sold lunch is incremented by one
         //    method returns cashGiven - lunch price 
         // if not enough money given, all is returned and nothing else happens
-        return -1;
+
+        if (cashGiven >= PRICE_OF_GOURMET_LUNCH){
+            this.cashInRegister += PRICE_OF_GOURMET_LUNCH;
+            gourmetSold++;
+            return cashGiven - PRICE_OF_GOURMET_LUNCH;
+        }
+        return cashGiven;
     }
 
+    public boolean payEconomical(LyyraCard lyycaCard) {
+        if (lyycaCard.balance() >= PRICE_OF_ECONOMICAL_LUNCH){
+            lyycaCard.pay(PRICE_OF_ECONOMICAL_LUNCH);
+            economicalSold++;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean payGourmet(LyyraCard lyycaCard) {
+        if (lyycaCard.balance() >=  PRICE_OF_GOURMET_LUNCH){
+            lyycaCard.pay(PRICE_OF_GOURMET_LUNCH);
+            gourmetSold++;
+            return true;
+        }
+        return false;
+    }
+
+    public void loadMoneyToCard(LyyraCard card, double sum) {
+        if(sum > 0){
+            card.loadMoney(sum);
+            this.cashInRegister += sum;
+            System.out.println("Card has been loaded. New balance on card is: " + card.balance());
+
+        }else {
+            System.out.println("The amount to be loaded must be positive.");
+        }
+    }
 
     public String toString() {
         return "money in register " + cashInRegister + " economical lunches sold: " + economicalSold + " gourmet lunches sold: " + gourmetSold;
--- TMCProjects/2013-OOProgrammingWithJava-PART1/week5-086.LyyraCardAndRegister/src/LyyraCard.java
+++ TMCProjects/2013-OOProgrammingWithJava-PART1/week5-086.LyyraCardAndRegister/src/LyyraCard.java
@@ -15,10 +15,20 @@ public class LyyraCard {
         this.balance += amount;
     }
 
+    /**
+     * Method checks if the balance of the card is at least amount given as parameter
+     * if not, methods returns false meaning that the card could not be used for the payment
+     * if the balance is enough, the given amount is taken from the balance and true is returned
+     *
+     * @param amount the amount that has to be subtracted
+     * @return true if amount has been taken from the balance, false otherwise.
+     */
+
     public boolean pay(double amount) {
-       // method checks if the balance of the card is at least amount given as parameter
-       // if not, methods returns false meaning that the card could not be used for the payment
-       // if the balance is enough, the given amount is taken from the balance and true is returned
+       if (this.balance >= amount){
+           this.balance -= amount;
+           return true;
+       }
         return false;
     }
 }
