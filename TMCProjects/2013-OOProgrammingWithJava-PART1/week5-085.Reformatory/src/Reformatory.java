600cf6b6ddc47080d2ac67bb2c6f940d03d54845
Aman Bahata <bahataaman@gmail.com>
2017-10-24 21:58:02 +0100

    Adds solution to exercise Reformatory

--- TMCProjects/2013-OOProgrammingWithJava-PART1/week5-085.Reformatory/src/Reformatory.java
+++ TMCProjects/2013-OOProgrammingWithJava-PART1/week5-085.Reformatory/src/Reformatory.java
@@ -1,8 +1,21 @@
 public class Reformatory {
 
+    private int weightMeasered = 0;
+
     public int weight(Person person) {
         // return the weight of the person
-        return -1;
+        weightMeasered++;
+        return person.getWeight();
     }
+
+    public void feed(Person person){
+        person.setWeight(person.getWeight() + 1);
+    }
+
+    public int totalWeightsMeasured(){
+        return  this.weightMeasered;
+    }
+
+
 
 }
