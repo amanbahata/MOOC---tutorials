534a74518cc814665bd2063b975396f981791f73
Aman Bahata <bahataaman@gmail.com>
2017-10-25 21:27:54 +0100

    Adds solution to exercise Students

--- TMCProjects/2013-OOProgrammingWithJava-PART1/week5-088.Students/src/Main.java
+++ TMCProjects/2013-OOProgrammingWithJava-PART1/week5-088.Students/src/Main.java
@@ -6,5 +6,41 @@ public class Main {
 
     public static void main(String[] args) {
         // write here the main program
+
+        Scanner reader = new Scanner(System.in);
+        String name;
+        String studentNumber;
+        ArrayList<Student> list = new ArrayList<Student>();
+
+        do {
+            System.out.print("name: ");
+            name = reader.nextLine();
+
+            if (!name.isEmpty()){
+                System.out.print("studentnumber: ");
+                studentNumber = reader.nextLine();
+                Student student = new Student(name, studentNumber);
+                list.add(student);
+            }
+        }while (!name.isEmpty());
+
+        System.out.println();
+
+        for (Student student : list) {
+            System.out.println(student.toString());
+        }
+
+        // Search
+        System.out.println();
+        System.out.print("Give search term: ");
+
+        String searchTerm = reader.nextLine();
+        System.out.println("Result: ");
+
+        for (Student student : list) {
+            if (student.getName().contains(searchTerm)){
+                System.out.println(student.toString());
+            }
+        }
     }
 }
