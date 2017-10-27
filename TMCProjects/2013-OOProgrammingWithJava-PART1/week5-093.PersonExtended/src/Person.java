5094b60372249598344753e501139c754b269335
Aman Bahata <bahataaman@gmail.com>
2017-10-27 01:33:59 +0100

    Adds solution to exercise PersonExtended

--- TMCProjects/2013-OOProgrammingWithJava-PART1/week5-093.PersonExtended/src/Person.java
+++ TMCProjects/2013-OOProgrammingWithJava-PART1/week5-093.PersonExtended/src/Person.java
@@ -1,4 +1,5 @@
 import java.util.Calendar;
+import java.util.Date;
 
 public class Person {
     private String name;
@@ -8,20 +9,37 @@ public class Person {
         this.name = name;
         this.birthday = new MyDate(pp, kk, vv);
     }
+
+    public Person(String name, MyDate birthday){
+        this.name = name;
+        this.birthday = birthday;
+    }
+
+    public Person(String name){
+        this.name = name;
+        this.birthday =  new MyDate(Calendar.getInstance().get(Calendar.DATE),
+                Calendar.getInstance().get(Calendar.MONTH) + 1, Calendar.getInstance().get(Calendar.YEAR));
+    }
     
     
     public int age() {
-// calculate the age based on the birthday and the current day
+        // calculate the age based on the birthday and the current day
         // you get the current day as follows: 
         // Calendar.getInstance().get(Calendar.DATE);
         // Calendar.getInstance().get(Calendar.MONTH) + 1; // January is 0 so we add one
         // Calendar.getInstance().get(Calendar.YEAR);
-        return 0;
+
+        MyDate today = new MyDate(Calendar.getInstance().get(Calendar.DATE),
+                Calendar.getInstance().get(Calendar.MONTH) + 1, Calendar.getInstance().get(Calendar.YEAR));
+
+        return today.differenceInYears(birthday);
     }
+
+
     
     public boolean olderThan(Person compared) {
         // compare the ages based on birthdays
-        return false;
+        return birthday.earlier(compared.birthday);
     }
     
     public String getName() {
