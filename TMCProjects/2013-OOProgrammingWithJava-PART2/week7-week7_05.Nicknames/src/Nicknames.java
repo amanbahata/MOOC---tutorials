36f84e879ca0ce7af86e559d874131c7fbd1defb
Aman Bahata <bahataaman@gmail.com>
2017-11-01 22:48:52 +0000

    Adds solution to exercise Nicknames

--- TMCProjects/2013-OOProgrammingWithJava-PART2/week7-week7_05.Nicknames/src/Nicknames.java
+++ TMCProjects/2013-OOProgrammingWithJava-PART2/week7-week7_05.Nicknames/src/Nicknames.java
@@ -6,6 +6,16 @@ public class Nicknames {
 
     public static void main(String[] args) {
         // Do the operations requested in the assignment here!
+
+        HashMap<String, String> nicknames = new HashMap<String, String>();
+
+        nicknames.put("matti","mage");
+        nicknames.put("mikael", "mixu");
+        nicknames.put("arto", "arppa");
+
+        String mickelNickname = nicknames.get("mikael");
+        System.out.println(mickelNickname);
+
     }
 
 }
